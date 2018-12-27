b4894d14ffcf9779ab1321eb716d3ec5e05417fa
kripesh <kripeshadwani@gmail.com>
2018-12-27 20:55:01 +0530

    Convert to MVC Design Pattern

---
+++ Todey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todey
+//
+//  Created by Kripesh Adwani on 27/12/18.
+//  Copyright © 2018 Kripesh Adwani. All rights reserved.
+//
+
+import Foundation
+
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
